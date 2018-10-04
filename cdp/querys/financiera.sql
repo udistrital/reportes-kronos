1c68589a4d4cb885e8ac2d11115b6175126985d7
Juan Camilo Sarmiento Reyes <yhomii.cloud@gmail.com>
2018-10-04 11:16:35 -0500

    Correcciones a reporte de CDP:
    * Se separo la información de los rubros, fuentes de financiamiento y objeto en tablas diferentes.
    
    * Se agrego texto de la solicitud de disponibilidad
    
    * Se agrego texto con el número de la necesida.
    
    * Se agrego un pie de página con la fecha de creación del reporte
    
    *Se ajusto la posición de los iconos
    
    * Se elimino la cabecera con el nombre "UNIVERSIDAD DISTRITTAL FRANCISCO JOSÉ DE CALDAS"

--- cdp/querys/financiera.sql
+++ cdp/querys/financiera.sql
@@ -18,10 +18,12 @@ AND dispe.proceso_externo = 12;
 
 
 /*
+
 SELECT * FROM financiera.disponibilidad_proceso_externo WHERE proceso_externo = 12;
 SELECT * FROM financiera.disponibilidad WHERE id = 101348;
 SELECT * FROM financiera.disponibilidad_apropiacion WHERE disponibilidad = 101348; --fuente: 98
 SELECT * FROM financiera.apropiacion WHERE id = 44529; 
 SELECT * FROM financiera.rubro WHERE id = 31834; --unidad_ejecutora: 1
 SELECT * FROM financiera.fuente_financiamiento;
+SELECT * FROM financiera.fuente_financiamiento WHERE id = 98;
 */
