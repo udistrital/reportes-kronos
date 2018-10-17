a23e5ab539c302a55acbba41122c07ceb744f5d0
Juan Camilo Sarmiento Reyes <jcamilosarmientor@gmail.com>
2018-10-17 08:14:25 -0500

    Reporte de RP incluyendo información adicional
    * Número de Compromiso
    * Tipo de Compromiso
    * Modalidad de Selección
    * id Beneficiario
    * Fecha generada del reporte
    
    Querys actualizadas

--- eclipse_neon/Kronos/rp/querys/administrativa.sql
+++ eclipse_neon/Kronos/rp/querys/administrativa.sql
@@ -1,13 +1,15 @@
-SELECT nec.*
+SELECT nec.*, molsec.nombre
 FROM 
 financiera.disponibilidad_apropiacion diapr, financiera.apropiacion apr, financiera.disponibilidad dis,
 financiera.rubro rub, administrativa.solicitud_disponibilidad soldis, administrativa.necesidad nec,
-financiera.registro_presupuestal_disponibilidad_apropiacion rpdis, financiera.registro_presupuestal rp
+financiera.registro_presupuestal_disponibilidad_apropiacion rpdis, financiera.registro_presupuestal rp,
+administrativa.modalidad_seleccion molsec
 WHERE apr.id = diapr.apropiacion 
 AND dis.id = diapr.disponibilidad
 AND rub.id = apr.rubro
 AND soldis.id = dis.solicitud
 AND soldis.necesidad = nec.id
+AND molsec.id = nec.modalidad_seleccion
 AND rpdis.disponibilidad_apropiacion = diapr.id
 AND rpdis.registro_presupuestal = rp.id 
 AND rp.vigencia = 2018
@@ -15,3 +17,8 @@ AND rub.unidad_ejecutora = 1
 AND diapr.fuente_financiamiento = 0
 AND rp.numero_registro_presupuestal = 21
 LIMIT 10;
+
+
+/*
+SELECT * FROM administrativa.modalidad_seleccion LIMIT 10;
+*/
--- eclipse_neon/Kronos/rp/querys/financiera.sql
+++ eclipse_neon/Kronos/rp/querys/financiera.sql
@@ -1,10 +1,14 @@
-/*
-SELECT dis.*
+SELECT rub.codigo, rub.nombre, rpdis.valor, rp.numero_registro_presupuestal, 
+dis.numero_disponibilidad, dis.solicitud, tcfi.nombre, rp.numero_compromiso, rp.beneficiario
 FROM financiera.registro_presupuestal rp
 INNER JOIN financiera.registro_presupuestal_disponibilidad_apropiacion rpdis
 ON rp.id = rpdis.registro_presupuestal
 INNER JOIN financiera.disponibilidad_apropiacion disapr
 ON rpdis.disponibilidad_apropiacion = disapr.id
+INNER JOIN financiera.compromiso_financiero cpfi
+ON cpfi.id = rp.tipo_compromiso
+INNER JOIN financiera.tipo_compromiso_financiero tcfi
+ON tcfi.id = cpfi.tipo_compromiso_financiero
 INNER JOIN financiera.disponibilidad dis
 ON dis.id = disapr.disponibilidad
 INNER JOIN financiera.apropiacion apr
@@ -14,8 +18,9 @@ ON rub.id = apr.rubro
 WHERE rp.vigencia = 2018
 AND rub.unidad_ejecutora = 1
 AND disapr.fuente_financiamiento = 0
-AND rp.numero_registro_presupuestal = 20
+AND rp.numero_registro_presupuestal = 20;
 
+/*
 SELECT * 
 FROM financiera.registro_presupuestal rp
 INNER JOIN financiera.registro_presupuestal_disponibilidad_apropiacion rpdis
@@ -31,21 +36,6 @@ LIMIT 10;
 SELECT * FROM financiera.disponibilidad_apropiacion LIMIt 10;
 SELECT * FROM financiera.registro_presupuestal LIMIt 10;
 SELECT * FROM financiera.registro_presupuestal_disponibilidad_apropiacion LIMIt 10;
-
-SELECT * 
-FROM 
-financiera.disponibilidad_apropiacion diapr, financiera.apropiacion apr, financiera.disponibilidad dis
-financiera.rubro rub, administrativa.solicitud_disponibilidad soldis, administrativa.necesidad nec,
-financiera.registro_presupuestal_disponibilidad_apropiacion rpdis, financiera.registro_presupuestal rp
-WHERE apr.id = diapr.apropiacion 
-AND dis.id = diapr.dsisponibilidad
-AND rub.id = apr.rubro
-AND soldis.necesidad = nec.id
-AND rpdis.disponibilidad_apropiacion = diapr.id
-AND rp.id = rpdis.registro_presupuestal
-AND rp.vigencia = 2018
-AND rub.unidad_ejecutora = 1
-AND diapr.fuente_financiamiento = 0
-AND rp.numero_registro_presupuestal = 2
-LIMIT 10
+SELECT * FROM financiera.compromiso_financiero limit 10;
+SELECT * FROM financiera.tipo_compromiso_financiero LIMIT 10;
 */
