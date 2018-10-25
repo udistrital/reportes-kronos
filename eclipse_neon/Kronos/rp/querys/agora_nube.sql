e3dc948617133b1219af46cd646eae5ec63dd067
Juan Camilo Sarmiento Reyes <yhomii.cloud@gmail.com>
2018-10-25 15:48:12 -0500

    Finalizando reporte de CRP
    * Se agrego saldo de CDP
    * Se agrego saldo de CRP
    * se ajusto el texto de las fuentes de financiamiento
    * Se ajusto el texto del beneficiario
    * Se corrigio el problema de traer el objeto del RP

--- eclipse_neon/Kronos/rp/querys/agora_nube.sql
+++ eclipse_neon/Kronos/rp/querys/agora_nube.sql
@@ -1,7 +1,42 @@
-
-SELECT id_proveedor, tipopersona, num_documento, nom_proveedor
+SELECT *
 FROM agora.informacion_proveedor WHERE id_proveedor = 3146;
+
+SELECT * 
+FROM agora.informacion_persona_natural WHERE num_documento_persona = '53021099';
+
+SELECT * 
+FROM agora.informacion_persona_juridica WHERE num_nit_empresa = '899999230';
+
+SELECT info_proveedor.* 
+FROM agora.informacion_persona_natural info_natural, 
+agora.informacion_persona_juridica info_juridica,
+agora.informacion_proveedor info_proveedor
+WHERE 
+info_natural.num_documento_persona = info_proveedor.num_documento
+OR info_juridica.num_nit_empresa = info_proveedor.num_documento
+AND info_proveedor.id_proveedor = 3146;
+
+SELECT *
+FROM agora.parametro_estandar ;
 
 /*
 SELECT * FROM agora.tipo_entidad;
+
+
+CREATE OR REPLACE FUNCTION infoProveedor()
+RETURNS character varying AS $tipopersona$
+DECLARE
+	tipopersona character varying;
+	num_documento numeric(10);
+	proveedor agora.informacion_proveedor;
+BEGIN
+   SELECT tipopersona INTO tipopersona, num_documento INTO num_documento
+   FROM agora.informacion_proveedor WHERE id_proveedor = 3146;
+   
+   RETURN tipopersona;
+END;
+$tipopersona$ LANGUAGE plpgsql;
+
+SELECT * FROM infoProveedor();
+
 */
--- eclipse_neon/Kronos/rp/querys/financiera.sql
+++ eclipse_neon/Kronos/rp/querys/financiera.sql
@@ -1,7 +1,9 @@
-SELECT rp.id, dis.id, 
-rub.codigo, rub.nombre, rpdis.valor, rp.numero_registro_presupuestal, 
-dis.numero_disponibilidad, dis.solicitud, tcfi.nombre, rp.numero_compromiso, rp.beneficiario,
-fue.nombre WHEN null THEN 0, fue.codigo WHEN null THEN 0
+SELECT rub.codigo, rub.nombre, rpdis.valor, rp.numero_registro_presupuestal, 
+(SUM(disapr.valor) - SUM(rpdis.valor)) SALDO_CDP, 
+(SUM(rpdis.valor) - SUM(op.valor_base)) SALDO_RP, 
+dis.numero_disponibilidad, dis.solicitud, tcfi.nombre tipo_compromiso, 
+rp.numero_compromiso, rp.beneficiario, fue.nombre fuente_nombre, 
+fue.codigo fuente_codigo
 
 FROM financiera.registro_presupuestal rp
 INNER JOIN financiera.registro_presupuestal_disponibilidad_apropiacion rpdis
@@ -22,11 +24,23 @@ INNER JOIN financiera.apropiacion apr
 ON apr.id = disapr.apropiacion
 INNER JOIN financiera.rubro rub
 ON rub.id = apr.rubro
+LEFT OUTER JOIN financiera.orden_pago_registro_presupuestal oprp
+ON oprp.registro_presupuestal = rp.id
+LEFT OUTER JOIN financiera.orden_pago op
+ON op.id = oprp.orden_pago
+
 WHERE rp.vigencia = 2018
 AND rub.unidad_ejecutora = 1
---AND disapr.fuente_financiamiento = 0
-AND rp.numero_registro_presupuestal = 2;
+AND rp.numero_registro_presupuestal = 2
 
+GROUP BY rub.codigo, rub.nombre, rpdis.valor, rp.numero_registro_presupuestal, 
+dis.numero_disponibilidad, dis.solicitud, tcfi.nombre , 
+rp.numero_compromiso, rp.beneficiario, fue.nombre , 
+fue.codigo ;
+
+SELECT * FROM financiera.orden_pago LIMIT 10;
+SELECT * FROM financiera.orden_pago_registro_presupuestal LIMIT 10;
+DELETE FROM financiera.orden_pago WHERE consecutivo = 25;
 /*
 SELECT * 
 FROM financiera.registro_presupuestal rp
@@ -39,6 +53,62 @@ ON apr.id = disapr.apropiacion
 INNER JOIN financiera.rubro rub
 ON rub.id = apr.rubro
 LIMIT 10;
+
+-- CONSULTA DEL REPORTE
+SELECT rub.codigo, rub.nombre, rpdis.valor, rp.numero_registro_presupuestal, 
+dis.numero_disponibilidad, dis.solicitud, tcfi.nombre tipo_compromiso, 
+rp.numero_compromiso, rp.beneficiario, fue.nombre fuente_nombre, 
+fue.codigo fuente_codigo
+
+FROM financiera.registro_presupuestal rp
+INNER JOIN financiera.registro_presupuestal_disponibilidad_apropiacion rpdis
+ON rp.id = rpdis.registro_presupuestal
+INNER JOIN financiera.disponibilidad_apropiacion disapr
+ON rpdis.disponibilidad_apropiacion = disapr.id
+
+LEFT OUTER JOIN financiera.fuente_financiamiento fue
+ON fue.id = disapr.fuente_financiamiento
+
+INNER JOIN financiera.compromiso_financiero cpfi
+ON cpfi.id = rp.tipo_compromiso
+INNER JOIN financiera.tipo_compromiso_financiero tcfi
+ON tcfi.id = cpfi.tipo_compromiso_financiero
+INNER JOIN financiera.disponibilidad dis
+ON dis.id = disapr.disponibilidad
+INNER JOIN financiera.apropiacion apr
+ON apr.id = disapr.apropiacion
+INNER JOIN financiera.rubro rub
+ON rub.id = apr.rubro
+LEFT OUTER JOIN financiera.orden_pago_registro_presupuestal oprp
+ON oprp.registro_presupuestal = rp.id
+LEFT OUTER JOIN financiera.orden_pago op
+ON op.id = oprp.orden_pago
+						 
+WHERE rp.vigencia = 2018
+AND rub.unidad_ejecutora = 1
+AND rp.numero_registro_presupuestal = 2;
+
+-- PRUEBA PARA SALDO DE RP
+SELECT (SUM(rpcdp.valor)-SUM(op.valor_base)) AS RP_DISPONIBLE,
+SUM(rpcdp.valor) AS total_rp, SUM(op.valor_base) AS total_op,
+rpcdp.valor, op.valor_base
+FROM financiera.registro_presupuestal rp 
+LEFT JOIN financiera.orden_pago_registro_presupuestal oprp
+ON rp.id = oprp.registro_presupuestal
+LEFT JOIN financiera.orden_pago op
+ON op.id = oprp.orden_pago
+INNER JOIN financiera.registro_presupuestal_disponibilidad_apropiacion rpcdp
+ON rp.id = rpcdp.registro_presupuestal
+INNER JOIN financiera.disponibilidad_apropiacion cdpapr
+ON cdpapr.id = rpcdp.disponibilidad_apropiacion
+INNER JOIN financiera.apropiacion apr
+ON apr.id = cdpapr.apropiacion
+INNER JOIN financiera.rubro rub
+ON rub.id = apr.rubro
+WHERE rp.numero_registro_presupuestal = 2824
+AND rp.vigencia = 2018
+AND rub.unidad_ejecutora = 1
+GROUP BY rpcdp.valor, op.valor_base, op.id, rp.id;
 
 SELECT * FROM financiera.disponibilidad_apropiacion LIMIt 10;
 SELECT * FROM financiera.registro_presupuestal LIMIt 10;
